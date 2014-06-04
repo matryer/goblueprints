23e045396ddba75b9fa2e6bceba22ae80a4286c6
Mat Ryer <mat@stretchr.com>
2014-06-03 19:05:23 -0600

    Added template

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -3,23 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"text/template"
 )
+
+// templ represents a single template
+type templ struct {
+	source string
+	templ  *template.Template
+}
+
+// Handle is a http.HandleFunc that renders this template.
+func (t *templ) Handle(w http.ResponseWriter, r *http.Request) {
+	if t.templ == nil {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.source)))
+	}
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 
 	// root
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-        <head>
-          <title>Chat</title>
-        </head>
-        <body>
-          Let's chat!
-        </body>
-      </html>
-    `))
-	})
+	http.HandleFunc("/", (&templ{source: "chat.html"}).Handle)
 
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
