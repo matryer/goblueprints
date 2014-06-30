267f62e7878e72528d957ffd8bce940e1cbc65e9
Mat Ryer <mat@stretchr.com>
2014-06-30 16:33:37 -0600

    added logout functionality

--- chapter3/chat/main.go
+++ chapter3/chat/main.go
@@ -60,6 +60,16 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 
 	// get the room going
 	go r.run()
