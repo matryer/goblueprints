19f55365d1ee7182e82ce8a1f4b484cbb60f534e
Mat Ryer <mat@stretchr.com>
2014-07-03 12:34:42 -0600

    Added file upload capabilities

---
+++ chapter3/chat/upload.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"path"
+)
+
+// uploaderHandler expects two fields to be posted, userid and avatarFile.
+func uploaderHandler(w http.ResponseWriter, req *http.Request) {
+	userId := req.FormValue("userid")
+	file, header, err := req.FormFile("avatarFile")
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+	filename := path.Join("avatars", userId+path.Ext(header.Filename))
+	err = ioutil.WriteFile(filename, data, 0777)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+	io.WriteString(w, "Successful")
+}
