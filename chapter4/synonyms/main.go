356c350c15c0fef3ea5e08755bb96d4a4fe1011c
Mat Ryer <mat@stretchr.com>
2014-07-10 10:02:45 -0600

    implemented new thesaurus package

--- chapter4/synonyms/main.go
+++ chapter4/synonyms/main.go
@@ -2,36 +2,29 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
+
 	"log"
-	"net/http"
 	"os"
+
+	"github.com/matryer/goblueprints/chapter4/thesaurus"
 )
 
 func main() {
 	apiKey := os.Getenv("BHT_APIKEY")
+	thesaurus := &thesaurus.BigHugh{APIKey: apiKey}
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		word := s.Text()
-		counter := 0
-		response, err := http.Get("http://words.bighugelabs.com/api/2/" + apiKey + "/" + word + "/json")
+		syns, err := thesaurus.Synonyms(word)
 		if err != nil {
 			log.Fatalln("Failed when looking for synonyms for \""+word+"\"", err)
 		}
-		var data map[string]interface{}
-		if json.NewDecoder(response.Body).Decode(&data) == nil {
-			for _, valuesMap := range data {
-				for _, values := range valuesMap.(map[string]interface{}) {
-					for _, value := range values.([]interface{}) {
-						fmt.Println(value)
-						counter++
-					}
-				}
-			}
-		}
-		if counter == 0 {
+		if len(syns) == 0 {
 			log.Fatalln("Couldn't find any synonyms for \"" + word + "\"")
+		}
+		for _, syn := range syns {
+			fmt.Println(syn)
 		}
 	}
 }
