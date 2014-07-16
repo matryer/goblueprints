60aed26fd02e2c75a31be8d8b7d7a59fa397e29b
Mat Ryer <mat@stretchr.com>
2014-07-16 12:33:32 -0600

    added ballot

---
+++ chapter5/webpoll/ballot.go
@@ -0,0 +1,84 @@
+package webpoll
+
+import "sync"
+
+// Ballot represents a source of votes.
+type Ballot interface {
+	// Start should begin collecting votes in the background
+	// and return a channel on which the votes will be reported.
+	// If Start returns an error, it should close the channel itself.
+	Start(options []string) (<-chan string, error)
+	// Stop should stop the ballot from collecting votes.
+	Stop()
+}
+
+// Ballots represents many Ballot objects.
+type Ballots []Ballot
+
+// Start starts all Ballots aggregating the results into a single
+// channel which is returned.
+// If any single Ballot fails to Start, the operation is aborted and
+// the error is returned.
+func (bs Ballots) Start(options []string) (<-chan string, error) {
+
+	out := make(chan string)
+
+	// keep track of started and finished ballots
+	startErrs := make(map[int]error)
+	var startX sync.Mutex
+	var start, finish sync.WaitGroup
+	start.Add(len(bs))
+	finish.Add(len(bs))
+
+	for i, ballot := range bs {
+		go func(i int, ballot Ballot) {
+			thisOut, err := ballot.Start(options)
+			if err != nil {
+				startX.Lock()
+				startErrs[i] = err // save the error
+				startX.Unlock()
+				start.Done() // started but failed
+			} else {
+				start.Done() // started
+				for vote := range thisOut {
+					out <- vote
+				}
+			}
+			finish.Done()
+		}(i, ballot)
+	}
+
+	go func() {
+		// wait for all ballots to finish
+		finish.Wait()
+		close(out)
+	}()
+
+	start.Wait() // wait for everything to have started
+
+	// any errors?
+	if len(startErrs) > 0 {
+		// stop any ballots that successfully started
+		var lastErr error
+		for i, _ := range bs {
+			if startErrs[i] == nil {
+				bs[i].Stop()
+			} else {
+				lastErr = startErrs[i]
+			}
+		}
+		return nil, lastErr
+	}
+
+	// no errors - return the channel
+	return out, nil
+}
+
+// Stop stops all Ballots from collecting votes and causes
+// the channel returned from Start to be closed.
+func (bs Ballots) Stop() error {
+	for _, ballot := range bs {
+		ballot.Stop()
+	}
+	return nil
+}
---
+++ chapter5/webpoll/ballot_test.go
@@ -0,0 +1,135 @@
+package webpoll_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/matryer/goblueprints/chapter5/webpoll"
+	"github.com/stretchr/testify/assert"
+)
+
+var ErrTest = errors.New("This is a test error")
+
+type TestBallot struct {
+	out       chan string
+	ShouldErr bool
+}
+
+// TestBallot must implement webpoll.Ballot
+var _ webpoll.Ballot = (*TestBallot)(nil)
+
+func (b *TestBallot) Start(options []string) (<-chan string, error) {
+	b.out = make(chan string)
+
+	if b.ShouldErr {
+		close(b.out)
+		return b.out, ErrTest
+	}
+
+	return b.out, nil
+}
+func (b *TestBallot) Stop() {
+	close(b.out)
+}
+func (b *TestBallot) Vote(option string) {
+	b.out <- option
+}
+
+func TestBallotImpl(t *testing.T) {
+
+	b := &TestBallot{}
+	out, _ := b.Start([]string{"one", "two", "three"})
+	if out == nil {
+		t.Error("Ballots.Start should return a channel")
+	}
+
+	// stop after 1 second
+	go func() {
+		time.Sleep(500 * time.Millisecond)
+		b.Stop()
+	}()
+
+	options := []string{"one", "two", "three"}
+
+	// simulate real data
+	go func() {
+		for _, v := range options {
+			b.Vote(v)
+		}
+	}()
+
+	var actualOptions []string
+	for option := range out {
+		actualOptions = append(actualOptions, option)
+	}
+
+	for i, _ := range options {
+		if options[i] != actualOptions[i] {
+			t.Error("Option", i, "should be", options[i], "but was", actualOptions[i])
+		}
+	}
+
+}
+
+func TestManyBallotStartErr(t *testing.T) {
+
+	b1 := &TestBallot{}
+	b2 := &TestBallot{}
+	b3 := &TestBallot{}
+
+	b2.ShouldErr = true
+
+	bs := webpoll.Ballots([]webpoll.Ballot{b1, b2, b3})
+	out, err := bs.Start([]string{"one", "two", "three"})
+
+	assert.Equal(t, ErrTest, err)
+	assert.Nil(t, out)
+
+}
+
+func TestManyBallot(t *testing.T) {
+
+	b1 := &TestBallot{}
+	b2 := &TestBallot{}
+	b3 := &TestBallot{}
+
+	bs := webpoll.Ballots([]webpoll.Ballot{b1, b2, b3})
+	out, _ := bs.Start([]string{"one", "two", "three"})
+
+	if out == nil {
+		t.Error("Ballots.Start should return a channel")
+	}
+
+	// stop after 1 second
+	go func() {
+		time.Sleep(500 * time.Millisecond)
+		bs.Stop()
+	}()
+
+	options := []string{"one", "two", "three"}
+	// simulate real data
+	go func() {
+		for _, v := range options {
+			b1.Vote(v)
+			b2.Vote(v)
+			b3.Vote(v)
+		}
+	}()
+
+	voteCount := make(map[string]int)
+	for option := range out {
+		voteCount[option]++
+	}
+
+	if voteCount["one"] != 3 {
+		t.Error("Expected 3 x one")
+	}
+	if voteCount["two"] != 3 {
+		t.Error("Expected 3 x two")
+	}
+	if voteCount["three"] != 3 {
+		t.Error("Expected 3 x three")
+	}
+
+}
