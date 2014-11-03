ab703eebaec73a2799d3a654b3fd223d2d0e2ea8
Mat Ryer <matryer@Mats-MacBook-Pro.local>
2014-11-03 13:00:52 -0700

    added command line tool

---
+++ chapter8/backup/cmds/backup/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/matryer/filedb"
+)
+
+/*
+  backup command
+
+  usage:
+
+    backup -db=./backupdata.db add {path} [{path} {path}...]
+    backup -db=./backupdata.db remove {path} [{path} {path}...]
+    backup -db=./backupdata.db list
+
+*/
+
+type path struct {
+	Path string
+	Hash string
+}
+
+func (p path) String() string {
+	return fmt.Sprintf("%s [%s]", p.Path, p.Hash)
+}
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			flag.PrintDefaults()
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		dbpath = flag.String("db", "./backupdata", "path to database directory")
+	)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		fatalErr = errors.New("invalid usage; must specify command")
+		return
+	}
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+	col, err := db.C("backup")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	cmd := strings.ToLower(args[0])
+	switch cmd {
+	case "list":
+		var path path
+		col.ForEach(func(i int, data []byte) bool {
+			err := json.Unmarshal(data, &path)
+			if err != nil {
+				fatalErr = err
+				return false
+			}
+			fmt.Printf("= %s\n", path)
+			return false
+		})
+	case "add":
+		if len(args[1:]) == 0 {
+			fatalErr = errors.New("must specify path to add")
+			return
+		}
+		for _, p := range args[1:] {
+			path := &path{Path: p, Hash: "Not yet archived"}
+			if err := col.InsertJSON(path); err != nil {
+				fatalErr = err
+				return
+			}
+			fmt.Printf("+ %s\n", path)
+		}
+	case "remove":
+		var path path
+		col.RemoveEach(func(i int, data []byte) (bool, bool) {
+			err := json.Unmarshal(data, &path)
+			if err != nil {
+				fatalErr = err
+				return false, true
+			}
+			for _, p := range args[1:] {
+				if path.Path == p {
+					fmt.Printf("- %s\n", path)
+					return true, false
+				}
+			}
+			return false, false
+		})
+	}
+
+	if fatalErr != nil {
+		return
+	}
+
+}
