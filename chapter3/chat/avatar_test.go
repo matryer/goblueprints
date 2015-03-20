33edbbc0375915d4827ca92998936c120d7e1a79
Mat Ryer <thatmatryer@gmail.com>
2015-03-20 12:39:28 +0000

    improved tests - fixes #1

--- chapter3/chat/avatar_test.go
+++ chapter3/chat/avatar_test.go
@@ -53,15 +53,20 @@ func TestFileSystemAvatar(t *testing.T) {
 
 	// make a test avatar file
 	filename := path.Join("avatars", "abc.jpg")
-	ioutil.WriteFile(filename, []byte{}, 0777)
-	defer func() { os.Remove(filename) }()
+	if err := os.Mkdir("avatars", 0777); err != nil {
+		t.Errorf("couldn't make avatar dir: %s", err)
+	}
+	if err := ioutil.WriteFile(filename, []byte{}, 0777); err != nil {
+		t.Errorf("couldn't make avatar: %s", err)
+	}
+	defer os.Remove(filename)
 
 	var fileSystemAvatar FileSystemAvatar
 	user := &chatUser{uniqueID: "abc"}
 
 	url, err := fileSystemAvatar.GetAvatarURL(user)
 	if err != nil {
-		t.Error("FileSystemAvatar.GetAvatarURL should not return an error")
+		t.Errorf("FileSystemAvatar.GetAvatarURL should not return an error: %s", err)
 	}
 	if url != "/avatars/abc.jpg" {
 		t.Errorf("FileSystemAvatar.GetAvatarURL wrongly returned %s", url)
