4159ce4444d33a25bfc104ca41ea40e13889ba90
Mat Ryer <thatmatryer@gmail.com>
2016-05-17 11:23:43 +0100

    minor improvements to locking code

--- chapter6/api/vars.go
+++ chapter6/api/vars.go
@@ -5,34 +5,34 @@ import (
 	"sync"
 )
 
+var l sync.RWMutex // protects vars
 var vars map[*http.Request]map[string]interface{}
-var varsLock sync.RWMutex
 
 // GetVar gets the value of the key for the specified http.Request.
 func GetVar(r *http.Request, key string) interface{} {
-	varsLock.RLock()
+	l.RLock()
 	value := vars[r][key]
-	varsLock.RUnlock()
+	l.RUnlock()
 	return value
 }
 
 // SetVar sets the key to the value for the specified http.Request.
 func SetVar(r *http.Request, key string, value interface{}) {
-	varsLock.Lock()
+	l.Lock()
 	vars[r][key] = value
-	varsLock.Unlock()
+	l.Unlock()
 }
 
 // OpenVars opens the vars for the specified http.Request.
 // Must be called before GetVar or SetVar is called for each
 // request.
 func OpenVars(r *http.Request) {
-	varsLock.Lock()
+	l.Lock()
 	if vars == nil {
-		vars = map[*http.Request]map[string]interface{}{}
+		vars = make(map[*http.Request]map[string]interface{})
 	}
 	vars[r] = map[string]interface{}{}
-	varsLock.Unlock()
+	l.Unlock()
 }
 
 // CloseVars closes the vars for the specified
@@ -40,7 +40,7 @@ func OpenVars(r *http.Request) {
 // Must be called when all var activity is completed to
 // clean up any used memory.
 func CloseVars(r *http.Request) {
-	varsLock.Lock()
+	l.Lock()
 	delete(vars, r)
-	varsLock.Unlock()
+	l.Unlock()
 }
