748e8fd609c17d423bec3cc91f191fdfc3c26d30
Mat Ryer <thatmatryer@gmail.com>
2016-07-20 09:33:41 +0100

    small improvements

--- chapter7/meander/journeys.go
+++ chapter7/meander/journeys.go
@@ -4,12 +4,14 @@ import (
 	"strings"
 )
 
-type j struct {
+// Journey represents a journey template.
+type Journey struct {
 	Name       string
 	PlaceTypes []string
 }
 
-func (j *j) Public() interface{} {
+// Public gets a public view of this Journey.
+func (j Journey) Public() interface{} {
 	return map[string]interface{}{
 		"name":    j.Name,
 		"journey": strings.Join(j.PlaceTypes, "|"),
@@ -17,13 +19,10 @@ func (j *j) Public() interface{} {
 }
 
 // Journeys represents the pre-set journeys data.
-var Journeys = []struct {
-	Name       string
-	PlaceTypes []string
-}{
-	{Name: "a romantic day", PlaceTypes: []string{"park", "bar", "movie_theatre", "restaurant", "florist", "taxi_stand"}},
-	{Name: "a shopping spree", PlaceTypes: []string{"department_store", "cafe", "clothing_store", "jewelry_store", "shoe_store"}},
-	{Name: "a night out", PlaceTypes: []string{"bar", "casino", "food", "bar", "night_club", "bar", "bar", "hospital"}},
-	{Name: "a culture day", PlaceTypes: []string{"museum", "cafe", "cemetery", "library", "art_gallery"}},
-	{Name: "a pamper day", PlaceTypes: []string{"hair_care", "beauty_salon", "cafe", "spa"}},
+var Journeys = []interface{}{
+	Journey{Name: "a romantic day", PlaceTypes: []string{"park", "bar", "movie_theatre", "restaurant", "florist", "taxi_stand"}},
+	Journey{Name: "a shopping spree", PlaceTypes: []string{"department_store", "cafe", "clothing_store", "jewelry_store", "shoe_store"}},
+	Journey{Name: "a night out", PlaceTypes: []string{"bar", "casino", "food", "bar", "night_club", "bar", "bar", "hospital"}},
+	Journey{Name: "a culture day", PlaceTypes: []string{"museum", "cafe", "cemetery", "library", "art_gallery"}},
+	Journey{Name: "a pamper day", PlaceTypes: []string{"hair_care", "beauty_salon", "cafe", "spa"}},
 }
