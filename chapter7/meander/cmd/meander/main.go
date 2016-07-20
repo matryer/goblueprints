a22752ee65b8ee21c055cf483339a3133d596733
Mat Ryer <thatmatryer@gmail.com>
2016-07-20 09:35:18 +0100

    moved command

---
+++ chapter7/meander/cmd/meander/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/matryer/goblueprints/chapter7/meander"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	meander.APIKey = "YOUR API KEY HERE"
+	http.HandleFunc("/journeys", cors(func(w http.ResponseWriter, r *http.Request) {
+		respond(w, r, meander.Journeys)
+	}))
+	http.HandleFunc("/recommendations", cors(func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lng, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	}))
+	log.Println("serving meander API on :8080")
+	http.ListenAndServe(":8080", http.DefaultServeMux)
+}
+
+func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
+	publicData := make([]interface{}, len(data))
+	for i, d := range data {
+		publicData[i] = meander.Public(d)
+	}
+	return json.NewEncoder(w).Encode(publicData)
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		f(w, r)
+	}
+}
